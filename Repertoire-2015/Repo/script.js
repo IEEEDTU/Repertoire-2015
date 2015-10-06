138e6f7f8b4b9db10846e2c87f7a1085f23bbddd
Abhilasha68 <abhilashasingh1996@gmail.com>
2015-10-06 22:41:25 +0530

    Slider changed to carousel
    Slider edited

---
+++ Repertoire-2015/Repo/script.js
@@ -0,0 +1,58 @@
+// jQuery Document
+
+$(document).ready(function() {
+	$('.event-content').hide();
+    $(".event-logo").click(function (e) {
+		var current = $(this).attr('data-event');
+		if($(e.target).is('.active')) {
+			$('.event-logo').removeClass('active');
+            closeAccordion();
+        }else {
+            closeAccordion();
+			$('.event-logo').removeClass('active');
+            $(this).addClass('active');
+            openAccordion(current);
+			$(current).fadeIn(500);
+        }
+	});
+	
+});
+
+function openAccordion(current) {
+	$('[data-pos="tl"]').animate({
+			top:"-100px",
+			left:"-100px" 
+  		}, 500);
+		$('[data-pos="tr"]').animate({
+			top:"-100px",
+			right:"-100px" 
+  		}, 500);
+		$('[data-pos="bl"]').animate({
+			bottom:"-100px",
+			left:"-100px" 
+  		}, 500);
+		$('[data-pos="br"]').animate({
+			bottom:"-100px",
+			right:"-100px" 
+  		}, 500);
+} 
+
+function closeAccordion() {
+	$('[data-pos="tl"]').animate({
+			top:"0px",
+			left:"0px" 
+  		}, 500);
+		$('[data-pos="tr"]').animate({
+			top:"0px",
+			right:"0px" 
+  		}, 500);
+		$('[data-pos="bl"]').animate({
+			bottom:"0px",
+			left:"0px" 
+  		}, 500);
+		$('[data-pos="br"]').animate({
+			bottom:"0px",
+			right:"0px" 
+  		}, 500);
+		$('.event-content').fadeOut(500);
+}
